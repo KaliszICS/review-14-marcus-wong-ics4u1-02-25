91ff72bbd92637057ba5343e96b9a27f3c1b06ce
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-02-27 15:03:09 +0000

    smae

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,5 +1,39 @@
 public class PracticeProblem {
+
+	final static int MAXIMUM = 10;
+
 	public static void main(String args[]) {
+
+	}
+	public static int calculate(int num, int num2, char c) {
+		switch (c) {
+			case '+':
+				return num + num2;
+				
+			case '-':
+				return num - num2;
+				
+			case '*':
+				return num * num2;
+				
+			case '/':
+				return num / num2;
+				
+			case '%':
+				return num % num2;
+				
+			case '^':
+				return (int)Math.pow(num, num2);
+			
+			default:
+				return -1;
+
+
+		}
+
+
+	}
+	public static boolean totalWordsChecker(String word) {
 
 	}
 
